0c60ebbef15d095a8e15dd448b73eec37b365d5d
dengruhao <1548731072@qq.com>
2018-10-08 23:23:22 +0800

    学儿歌、学跳舞界面布局完善

--- happyball/app/src/main/java/cn/baby/happyball/vedio/VedioDanceActivity.java
+++ happyball/app/src/main/java/cn/baby/happyball/vedio/VedioDanceActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -14,26 +16,62 @@ import cn.baby.happyball.R;
 
 public class VedioDanceActivity extends BaseActivity {
 
-    @BindView(R.id.iv_back)
-    ImageView ivBack;
+    @BindView(R.id.rl_homepage)
+    RelativeLayout rlHomePage;
     @BindView(R.id.iv_homepage)
     ImageView ivHomePage;
-    @BindView(R.id.iv_dance_boom)
-    ImageView ivDanceBoom;
-    @BindView(R.id.iv_dance_sky)
-    ImageView ivDanceSky;
-    @BindView(R.id.iv_dance_dark)
-    ImageView ivDanceDark;
-    @BindView(R.id.iv_dance_clouds)
-    ImageView ivDanceClouds;
-    @BindView(R.id.iv_dance_safe)
-    ImageView ivDanceSafe;
-    @BindView(R.id.iv_dance_ipad)
-    ImageView ivDanceIpad;
-    @BindView(R.id.iv_dance_computer)
-    ImageView ivDanceComputer;
-    @BindView(R.id.iv_dance_thunder)
-    ImageView ivDanceThumber;
+    @BindView(R.id.rl_back)
+    RelativeLayout rlBack;
+    @BindView(R.id.iv_back)
+    ImageView ivBack;
+    @BindView(R.id.rl_dance_first)
+    RelativeLayout rlDanceFirst;
+    @BindView(R.id.iv_dance_first)
+    ImageView ivDanceFirst;
+    @BindView(R.id.tv_dance_first)
+    TextView tvDanceFirst;
+    @BindView(R.id.rl_dance_second)
+    RelativeLayout rlDanceSecond;
+    @BindView(R.id.iv_dance_second)
+    ImageView ivDanceSecond;
+    @BindView(R.id.tv_dance_second)
+    TextView tvDanceSecond;
+    @BindView(R.id.rl_dance_third)
+    RelativeLayout rlDanceThird;
+    @BindView(R.id.iv_dance_third)
+    ImageView ivDanceThird;
+    @BindView(R.id.tv_dance_third)
+    TextView tvDanceThird;
+    @BindView(R.id.rl_dance_four)
+    RelativeLayout rlDanceFour;
+    @BindView(R.id.iv_dance_four)
+    ImageView ivDanceFour;
+    @BindView(R.id.tv_dance_four)
+    TextView tvDanceFour;
+    @BindView(R.id.rl_dance_five)
+    RelativeLayout rlDanceFive;
+    @BindView(R.id.iv_dance_five)
+    ImageView ivDanceFive;
+    @BindView(R.id.tv_dance_five)
+    TextView tvDanceFive;
+    @BindView(R.id.rl_dance_six)
+    RelativeLayout rlDanceSix;
+    @BindView(R.id.iv_dance_six)
+    ImageView ivDanceSix;
+    @BindView(R.id.tv_dance_six)
+    TextView tvDanceSix;
+    @BindView(R.id.rl_dance_seven)
+    RelativeLayout rlDanceSeven;
+    @BindView(R.id.iv_dance_seven)
+    ImageView ivDanceSeven;
+    @BindView(R.id.tv_dance_seven)
+    TextView tvDanceSeven;
+    @BindView(R.id.rl_dance_eight)
+    RelativeLayout rlDanceEight;
+    @BindView(R.id.iv_dance_eight)
+    ImageView ivDanceEight;
+    @BindView(R.id.tv_dance_eight)
+    TextView tvDanceEight;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -42,12 +80,12 @@ public class VedioDanceActivity extends BaseActivity {
         ButterKnife.bind(this);
     }
 
-    @OnClick(R.id.iv_back)
+    @OnClick({R.id.iv_back, R.id.rl_back})
     public void onBack() {
         startActivity(new Intent(VedioDanceActivity.this, VedioFinishActivity.class));
     }
 
-    @OnClick(R.id.iv_homepage)
+    @OnClick({R.id.iv_homepage, R.id.rl_homepage})
     public void onHomepage() {
         startActivity(new Intent(VedioDanceActivity.this, MainActivity.class));
     }
--- happyball/app/src/main/java/cn/baby/happyball/vedio/VedioSongActivity.java
+++ happyball/app/src/main/java/cn/baby/happyball/vedio/VedioSongActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -14,14 +16,38 @@ import cn.baby.happyball.R;
 
 public class VedioSongActivity extends BaseActivity {
 
-    @BindView(R.id.iv_back)
-    ImageView ivBack;
+    @BindView(R.id.rl_homepage)
+    RelativeLayout rlHomaPage;
     @BindView(R.id.iv_homepage)
     ImageView ivHomePage;
+    @BindView(R.id.rl_back)
+    RelativeLayout rlBack;
+    @BindView(R.id.iv_back)
+    ImageView ivBack;
+    @BindView(R.id.rl_song_accompaniment)
+    RelativeLayout rlSongAccompaniment;
     @BindView(R.id.iv_song_accompaniment)
     ImageView ivSongAccompaniment;
+    @BindView(R.id.rl_song_sing)
+    RelativeLayout rlSongSing;
     @BindView(R.id.iv_song_sing)
     ImageView ivSongSing;
+    @BindView(R.id.tv_song_one_first)
+    TextView tvSongOneFirst;
+    @BindView(R.id.tv_song_one_second)
+    TextView tvSongOneSecond;
+    @BindView(R.id.tv_song_one_third)
+    TextView tvSongOneThird;
+    @BindView(R.id.tv_song_one_four)
+    TextView tvSongOneFour;
+    @BindView(R.id.tv_song_two_first)
+    TextView tvSongTwoFirst;
+    @BindView(R.id.tv_song_two_second)
+    TextView tvSongTwoSecond;
+    @BindView(R.id.tv_song_two_third)
+    TextView tvSongTwoThird;
+    @BindView(R.id.tv_song_two_four)
+    TextView tvSongTwoFour;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,22 +56,22 @@ public class VedioSongActivity extends BaseActivity {
         ButterKnife.bind(this);
     }
 
-    @OnClick(R.id.iv_back)
+    @OnClick({R.id.iv_back, R.id.rl_back})
     public void onBack() {
         startActivity(new Intent(VedioSongActivity.this, VedioFinishActivity.class));
     }
 
-    @OnClick(R.id.iv_homepage)
+    @OnClick({R.id.iv_homepage, R.id.rl_homepage})
     public void onHomepage() {
         startActivity(new Intent(VedioSongActivity.this, MainActivity.class));
     }
 
-    @OnClick(R.id.iv_song_accompaniment)
+    @OnClick({R.id.iv_song_accompaniment, R.id.rl_song_accompaniment})
     public void onSongAccompaniment() {
 
     }
 
-    @OnClick(R.id.iv_song_sing)
+    @OnClick({R.id.iv_song_sing, R.id.rl_song_sing})
     public void onSongSing() {
 
     }
